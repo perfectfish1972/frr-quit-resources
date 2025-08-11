e704fdfbc6c7c570dfdf57444c810af14bd9ac24
perfectfish1972 <perfectfish1972@yahoo.ca>
2025-08-11 20:07:21 -0300

    FRR Quit Resources web app

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+// Simple cache for offline basics
+const CACHE_NAME = 'frr-quit-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
